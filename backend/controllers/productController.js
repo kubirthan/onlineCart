4341d04df28afaa21b996dfda1ac2477578a16d9
kubirthan <kubirthan7991@gmail.com>
2024-09-18 20:46:30 +0530

    creating error middleware

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require('../utils/errorHandler')
 
 //Get all products - /api/v1/products
 exports.getProducts = async (req, res, next) => {
@@ -26,10 +27,7 @@ exports.getSingleProduct = async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
   if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: "Product not found",
-    });
+    return next(new ErrorHandler("product not found", 400))
   }
 
   res.status(200).json({
