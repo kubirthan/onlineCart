af63ccd298d7a8feb731727c21abf900d45be077
kubirthan <kubirthan7991@gmail.com>
2024-09-18 18:33:11 +0530

    Update Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,41 +1,61 @@
-const Product = require('../models/productModel')
-
+const Product = require("../models/productModel");
 
 //Get all products - /api/v1/products
-exports.getProducts = async(req, res, next) => {
-    const products = await Product.find()
-    let count = products.length
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
+  let count = products.length;
 
-    res.status(200).json({
-        success: true,
-        count, 
-        products
-    })
-}
-
+  res.status(200).json({
+    success: true,
+    count,
+    products,
+  });
+};
 
 //create new product - /api/v1/product/new
-exports.newProduct = async(req, res, next) => {
-    const product = await Product.create(req.body)
-    res.status(200).json({
-        success: true,
-        product
-    })
-}
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
 
-//Get Single Product - /api/v1/:id
-exports.getSingleProduct = async(req, res, next) => {
-    const product = await Product.findById(req.params.id)
+//Get Single Product - /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-    if(!product){
-        return res.status(404).json({
-            success: false,
-            message: "Product not found"
-        })
-    }
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
 
-    res.status(200).json({
-        success: true,
-        product
-    })
-}
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+//Update product - /api/v1/product/:id
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+};
