1ec8e7068ba24b9980fc3d45ba6b0583fa478083
kubirthan <kubirthan7991@gmail.com>
2024-09-18 16:23:18 +0530

    Get Single Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,3 +22,20 @@ exports.newProduct = async(req, res, next) => {
         product
     })
 }
+
+//Get Single Product - /api/v1/:id
+exports.getSingleProduct = async(req, res, next) => {
+    const product = await Product.findById(req.params.id)
+
+    if(!product){
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
