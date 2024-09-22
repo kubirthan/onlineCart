87a0928aab37ba93d855548ee53e0cbc632ed5bf
kubirthan <kubirthan7991@gmail.com>
2024-09-22 22:01:59 +0530

    Authorize user roles

--- backend/middlewares/authenticate.js
+++ backend/middlewares/authenticate.js
@@ -13,5 +13,13 @@ exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
     req.user = await User.findById(decoded.id)
     next()
-
 })
+
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorHandler(`Role ${req.user.role} is not allowed`,401))
+        }
+        next()
+    }
+}
