59ea6beab1011deca0442e65c1195eb776939b50
kubirthan <kubirthan7991@gmail.com>
2024-09-22 23:24:31 +0530

    Adding user in product schema

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -20,6 +20,7 @@ exports.getProducts = catchAsyncError(async (req, res, next) => {
 
 //create new product - /api/v1/product/new
 exports.newProduct = catchAsyncError(async (req, res, next) => {
+  req.body.user = req.user.id
   const product = await Product.create(req.body);
   res.status(200).json({
     success: true,
