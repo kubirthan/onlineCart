d9c14f484c0fc17740285f87954e0e1136721619
kubirthan <kubirthan7991@gmail.com>
2024-10-04 22:14:57 +0530

    Get loggedin user orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,49 +1,68 @@
-const catchAsyncError = require('../middlewares/catchAsyncError')
-const Order = require('../models/orderModel')
-const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncError = require("../middlewares/catchAsyncError");
+const Order = require("../models/orderModel");
+const ErrorHandler = require("../utils/errorHandler");
 
 //create new order - /api/v1/order/new
-exports.newOrder = catchAsyncError(async(req, res, next) => {
-    const {
-        
-        orderItems,
-        shippingInfo,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-        paymentinfo
+exports.newOrder = catchAsyncError(async (req, res, next) => {
+  const {
+    orderItems,
+    shippingInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paymentinfo,
+  } = req.body;
 
-    } = req.body
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paymentinfo,
+    paidAt: Date.now(),
+    user: req.user.id,
+  });
 
-    const order = await Order.create({
-        shippingInfo,
-        orderItems,
-        itemsPrice,
-        taxPrice,
-        shippingPrice,
-        totalPrice,
-        paymentinfo,
-        paidAt: Date.now(),
-        user: req.user.id
-    })
-
-    res.status(200).json({
-        success: true,
-        order
-    })
-})
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
 
 //Get single order - /api/v1/:id
-exports.getSingleOrder = catchAsyncError(async(req, res, next) => {
-    const order = await Order.findById(req.params.id).populate('user', 'name email')
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
 
-    if(!order){
-        return next(new ErrorHandler(`Order is not found with this id: ${req.params.id}`))
-    }
+  if (!order) {
+    return next(
+      new ErrorHandler(`Order is not found with this id: ${req.params.id}`)
+    );
+  }
 
-    res.status(200).json({
-        success: true,
-        order
-    })
-})
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+//Get loggedin user orders -/api/v1/myorders
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user.id });
+
+  if (!orders) {
+    return next(
+      new ErrorHandler(`Order is not found with this id: ${req.params.id}`)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    orders
+  })
+});
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const {isAuthenticatedUser} = require('../middlewares/authenticate')
-const { newOrder, getSingleOrder } = require('../controllers/orderController')
+const { newOrder, getSingleOrder, myOrders } = require('../controllers/orderController')
 
 
 router.route('/order/new').post(isAuthenticatedUser, newOrder)
 router.route('/order/:id').get(isAuthenticatedUser,getSingleOrder)
+router.route('/myorders').get(isAuthenticatedUser, myOrders)
 
 module.exports = router
