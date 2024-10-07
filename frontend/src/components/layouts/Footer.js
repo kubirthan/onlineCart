7e6aa2aca1fb5db2f82e87ead119319f2c052387
kubirthan <kubirthan7991@gmail.com>
2024-10-07 20:10:27 +0530

    Header & Footer create

---
+++ frontend/src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <footer class="py-1">
+      <p class="text-center text-dark mt-1">
+        JVLcart - 2022-2023, All Rights Reserved
+      </p>
+    </footer>
+    )
+}
