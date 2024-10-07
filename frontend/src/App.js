6278f1b38901991065e81eb587da61ce9cb22c55
kubirthan <kubirthan7991@gmail.com>
2024-10-07 20:24:07 +0530

    Create Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './components/Home';
 import Footer from './components/layouts/Footer';
 import Header from './components/layouts/Header';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
       <Footer/>
     </div>
   );
