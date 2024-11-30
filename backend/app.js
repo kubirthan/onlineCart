685b082fc7f383239fa385cacfe6fed6ad22cca4
kubirthan <kubirthan7991@gmail.com>
2024-11-30 17:59:34 +0530

    Backend Stripe Integration

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,9 @@ const app = express()
 const errorMiddleware = require('./middlewares/error')
 const cookieParser = require('cookie-parser')
 const path = require('path')
+const dotenv = require('dotenv')
+
+dotenv.config({path: path.join(__dirname,"/config/config.env")})
 
 app.use(express.json())
 app.use(cookieParser())
@@ -11,10 +14,12 @@ app.use('/uploads', express.static(path.join(__dirname,'uploads')))
 const products = require('./routes/product')
 const auth = require('./routes/auth')
 const order = require('./routes/order')
+const payment = require('./routes/payment')
 
 app.use('/api/v1',products)
 app.use('/api/v1',auth)
 app.use('/api/v1',order)
+app.use('/api/v1',payment)
 
 app.use(errorMiddleware)
 
